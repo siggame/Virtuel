cdf9ceda06d5975ed75f7ae3be077f5316c54f57
Unknown <russleyshaw@gmail.com>
2016-11-09 10:58:02 -0600

    Update scripts

--- game/generateSSHKey.sh
+++ game/generateSSHKey.sh
@@ -8,9 +8,3 @@ echo "and paste the key between the lines"
 echo "========================================"
 cat ~/.ssh/id_rsa.pub
 echo "========================================"
-xclip -sel clip < ~/.ssh/id_rsa.pub
-sensible-browser https://github.com/settings/keys &
-
-read -p "Press ENTER to exit..."
-
-
